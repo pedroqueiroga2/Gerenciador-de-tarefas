79328a436891873711dc1700b9ff746d3531be17
pedroqueiroga <pedroqueirogahub@gmail.com>
2025-02-10 10:44:13 -0300

    upando os arquivos

---
+++ Gerenciador de tarefas/Gerenciador.cs
@@ -0,0 +1,12 @@
+namespace Gerenciador
+{
+    public class Gerencia
+    {
+        private List<string> tarefas;
+
+        public void AdicionarTarefa(string tarefas)
+        {
+            tare
+        }
+    }
+}
